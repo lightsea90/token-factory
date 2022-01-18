e28476f08844b62fd8b7417a9039e2b63470cfe2
Harry Nguyen <harrynguyen0991@gmail.com>
2022-01-19 05:58:15 +0700

    Add unregister function

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -204,6 +204,15 @@ impl TokenFactory {
         );
         self.tokens.clear();
     }
+
+    pub fn unregister(&mut self, ft_contract: AccountId) {
+        assert!(env::state_exists(), "The contract is not initialized");
+        assert!(
+            env::current_account_id() == env::signer_account_id(), 
+            "Function called not from the contract owner itself",
+        );
+        self.tokens.remove(&ft_contract);
+    }
 }
 
 #[near_bindgen]
